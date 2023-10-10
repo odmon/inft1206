efa699670258ba2edfa1ec5ddc14ca3148000e7b
Andres Suarez <watatxumi@outlook.com>
2023-10-10 13:55:52 -0400

    Update main.js

--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -1,4 +1,4 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+//1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -9,7 +9,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
+//2. RAW TEXT STRINGS
 
 It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
@@ -25,7 +25,7 @@ spontaneously combusted
 melted into a puddle on the sidewalk
 turned into a slug and crawled away
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+//3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
