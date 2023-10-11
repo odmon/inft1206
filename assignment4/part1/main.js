eafca61c67afea5d4e99469ed7ad595ba5978cda
Andres Suarez <watatxumi@outlook.com>
2023-10-11 07:24:28 -0400

    Update main.js
    fix ;

--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -12,7 +12,7 @@ function randomValueFromArray(array){
 //2. RAW TEXT STRINGS
 
 //story base string
-const storyText='It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+const storyText='It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
 //array to insertx
 const insertx=['Willy the Goblin','Big Daddy','Father Christmas'];
