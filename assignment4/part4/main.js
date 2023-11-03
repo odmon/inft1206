6322f988fec73ea148099a7f4a2cc47a4863d04c
Andres Suarez <watatxumi@outlook.com>
2023-11-03 13:52:37 -0400

    add code keydown and hmtl p
    add event keydown
    
    html <p>

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -117,3 +117,23 @@ function loop() {
 
 //call loop
 loop();
+
+
+
+//new code evilcircle
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
