0aaca25a789ebbfbc4ffb8a8fada4c7ed9d8327c
Andres Suarez <watatxumi@outlook.com>
2023-11-03 13:36:35 -0400

    Update main.js
    collision detection small update

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -62,10 +62,10 @@ update() {
 }
 
 
-//collision detection
+//collision detection - small update
 collisionDetect() {
   for (const ball of balls) {
-    if (this !== ball) {
+    if (!(this === ball) && ball.exists) {
       const dx = this.x - ball.x;
       const dy = this.y - ball.y;
       const distance = Math.sqrt(dx * dx + dy * dy);
@@ -76,7 +76,6 @@ collisionDetect() {
     }
   }
 }
-
 }
 
 
