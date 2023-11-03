a0e3e51f4ee2774477091e6f900c9366c362b0a2
Andres Suarez <watatxumi@outlook.com>
2023-11-03 13:03:18 -0400

    Update main.js

--- assignment4/part3/main.js
+++ assignment4/part3/main.js
@@ -18,3 +18,56 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+
+//class ball
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+}
+
+//draw method to the ball class
+draw() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+//create a new ball instance
+const testBall = new Ball(50, 100, 4, 4, "blue", 10);
+
+//calling balls
+
+testBall.x;
+testBall.size;
+testBall.color;
+testBall.draw();
+
+//update ball's data
+update() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
